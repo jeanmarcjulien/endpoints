28fee16884869eb43987bf7c140cbbdf3858b780
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-06-10 17:23:04 -0500

    Update PagesController.java

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -2,92 +2,64 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
-public class PagesController {
+@RequestMapping("/api")
+public class XValueController {
 
-    String x="0";
+    private double xValue = 0;
 
     @GetMapping("/get_x")
-    public String getX(){
-        return x;
+    public XValueResponse getXValue() {
+        return new XValueResponse(xValue);
     }
 
     @PostMapping("/set_x")
-    public void setX(@RequestParam String x){
-        x=x;
+    public MessageResponse setXValue(@RequestBody XValueRequest request) {
+        this.xValue = request.getX();
+        return new MessageResponse("X value set successfully");
+    }
+}
+
+class XValueResponse {
+    private double x;
+
+    public XValueResponse(double x) {
+        this.x = x;
     }
 
-    @GetMapping("/greeting")
-    public String getQueryStrings(@RequestParam String first, @RequestParam String last){
-
-        return "hello " + first + " " + last + " sending query string data version 3";
+    public double getX() {
+        return x;
     }
 
-    @GetMapping("/greeting/{first}/{last}")
-    public String getPathVariables(@PathVariable String first, @PathVariable String last){
+    public void setX(double x) {
+        this.x = x;
+    }
+}
 
-        return "hello " + first + " " + last + " sending path variable data";
+class XValueRequest {
+    private double x;
+
+    public double getX() {
+        return x;
     }
 
-    @PostMapping("/greeting")
-    //@RequestMapping(value="/greeting", method = RequestMethod.POST)
-    public String getFormData(@RequestParam String first, @RequestParam String last){
-        return "hello " + first + " " + last + " sending form data";
+    public void setX(double x) {
+        this.x = x;
+    }
+}
+
+class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
     }
 
-    //Render a JSON object in a response
-
-    public static class Person {
-        private String firstName;
-        private String lastName;
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
+    public String getMessage() {
+        return message;
     }
 
-    @GetMapping("/json-object")
-    public Person getJsonObject() {
-        Person person = new Person();
-        person.firstName = "jean-marc";
-        person.lastName = "julien";
-        return person;
-    }
-
-    //Render a list of JSON objects in a response
-
-    @GetMapping("/json-array")
-    public List<Person> getJsonArray() {
-        Person person1 = new Person();
-        person1.firstName = "jean-marc";
-        person1.lastName = "julien";
-
-        Person person2 = new Person();
-        person2.firstName = "mike";
-        person2.lastName = "gaffney";
-
-        return Arrays.asList(person1, person2);
-    }
-
-    //Access JSON Request Data
-
-    @PostMapping("/json-data")
-    public String getJSONData(@RequestBody Person person) {
-        return "hello " + person.firstName + " " + person.lastName;
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
